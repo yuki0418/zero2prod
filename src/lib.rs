ffaf3017a5cdc9582f4e4c0fc3ce0d80bbc66cc3
yuki0418 <keryuuki0418@gmail.com>
2024-02-02 20:28:25 +0900

    Create database pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,3 @@
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use std::net::TcpListener;
-
 pub mod configuration;
 pub mod routes;
 pub mod startup;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
