145165f115cf77d936236d2bf80730c89b724971
yuki0418 <keryuuki0418@gmail.com>
2024-02-05 21:50:28 +0900

    Add test for domain perse

--- src/domain.rs
+++ src/domain.rs
@@ -5,6 +5,7 @@ pub struct NewSubscriber {
     pub name: SubscriberName,
 }
 
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl SubscriberName {
@@ -20,17 +21,17 @@ impl SubscriberName {
         &self.0
     }
 
-    pub fn parse(s: String) -> SubscriberName {
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
         let is_empty_or_whitespace = s.trim().is_empty();
         let is_too_long = s.graphemes(true).count() > 256;
         let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
         let contain_forbidden_characters =
             s.chars().any(|char| forbidden_characters.contains(&char));
 
-        if is_empty_or_whitespace && is_too_long && contain_forbidden_characters {
-            panic!("Subscriber name {} is invalid.", s);
+        if is_empty_or_whitespace || is_too_long || contain_forbidden_characters {
+            Err(format!("Subscriber name {} is invalid.", s))
         } else {
-            Self(s)
+            Ok(Self(s))
         }
     }
 }
@@ -38,5 +39,47 @@ impl SubscriberName {
 impl AsRef<str> for SubscriberName {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claims::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "a".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_invalid_characters_are_rejected() {
+        let name = "a".repeat(256) + ">";
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "a".repeat(255);
+        assert_ok!(SubscriberName::parse(name));
     }
 }
