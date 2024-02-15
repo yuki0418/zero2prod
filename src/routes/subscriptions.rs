d08d7d9df4034c771eb01fe24d88d0f4faa0572d
yuki0418 <keryuuki0418@gmail.com>
2024-02-15 17:32:38 +0900

    Refactoring subscriptions

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,22 @@ pub struct FormData {
     name: String,
 }
 
+pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
+    let name = SubscriberName::parse(form.name)?;
+    let email = SubscriberEmail::parse(form.email)?;
+    Ok(NewSubscriber { email, name })
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
+}
+
 #[tracing::instrument(
   name = "Adding a new subscriber.",
   skip(form, pool),
@@ -22,15 +38,15 @@ pub struct FormData {
   )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    // let new_subscriber = match parse_subscriber(form.0) {
+    //     Ok(subscriber) => subscriber,
+    //     Err(_) => return HttpResponse::BadRequest().finish(),
+    // };
+
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
